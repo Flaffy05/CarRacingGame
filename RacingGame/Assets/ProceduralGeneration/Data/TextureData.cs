53f25d3485566493c4014bfc02cd39855fb930a6
Fla <105724877+Flaffy05@users.noreply.github.com>
2023-11-26 22:11:26 +0100

    _

--- RacingGame/Assets/ProceduralGeneration/Data/TextureData.cs
+++ RacingGame/Assets/ProceduralGeneration/Data/TextureData.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor.Timeline.Actions;
 using UnityEngine;
 
+[CreateAssetMenu]
 public class TextureData : UpdatableData
 {
     public void ApplyToMaterial(Material material)
